b6477eade08c1f45dd903650922315fd4b2d2eb6
giaan7904 <anheo13012017@gmail.com>
2024-05-21 10:38:13 +0700

    commit

--- ss8/bt10.js
+++ ss8/bt10.js
@@ -1,8 +1,2 @@
 "use strict";
-const employee = {
-    name: "John",
-    age: 30,
-    addres: "123 Main St",
-    employeeId: "E12345"
-};
-console.log(employeee);
+//console.log(employeee)
--- ss8/bt10.ts
+++ ss8/bt10.ts
@@ -9,11 +9,6 @@ interface Person {
 interface Employee extends Person {
     employeeId: string;
 }
-const employee: Employee = {
-    name: "John",
-    age: 30,
-    addres: "123 Main St", 
-    employeeId: "E12345"
-};
 
-console.log(employeee)
+
+//console.log(employeee)
